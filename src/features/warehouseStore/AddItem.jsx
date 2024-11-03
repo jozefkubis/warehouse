d5090e4656afff25e4621a7f7ca41a5213224061
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-11-03 20:59:40 +0100

    Konvertovany modal na compound component

--- src/features/warehouseStore/AddItem.jsx
+++ src/features/warehouseStore/AddItem.jsx
@@ -1,23 +1,35 @@
-import { useState } from "react"
 import Modal from "../../ui/Modal"
 import Button from "../../ui/Button"
 import CreateStoreForm from "./CreateStoreForm"
 
 function AddItem() {
-  const [isOpenModal, setIsOpenModal] = useState(false)
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new Item to Store
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateStoreForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="item-form">
+        <Button>Add new Item to Store</Button>
+      </Modal.Open>
+      <Modal.Window name="item-form">
+        <CreateStoreForm />
+      </Modal.Window>
+    </Modal>
   )
 }
+
+// function AddItem() {
+//   const [isOpenModal, setIsOpenModal] = useState(false)
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new Item to Store
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateStoreForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   )
+// }
 
 export default AddItem

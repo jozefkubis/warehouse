ca5a230b7e00f1eeed3432a2a85bf086f43f6a99
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-12-13 19:55:27 +0100

    pridany error-boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,13 @@
 import { createRoot } from "react-dom/client"
+import { ErrorBoundary } from "react-error-boundary"
+import ErrorFallback from "./ui/ErrorFallback.jsx"
 import App from "./App.jsx"
 
-createRoot(document.getElementById("root")).render(<App />)
+createRoot(document.getElementById("root")).render(
+  <ErrorBoundary
+    FallbackComponent={ErrorFallback}
+    onReset={() => window.location.replace("/")}
+  >
+    <App />
+  </ErrorBoundary>
+)

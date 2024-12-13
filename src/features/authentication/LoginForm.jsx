954eef093281213f280c98db561d3ffe52284fba
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-12-13 20:06:12 +0100

    kozmeticka uprava v LoginForm

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin"
 import SpinnerMini from "../../ui/SpinnerMini"
 
 function LoginForm() {
-  const [email, setEmail] = useState("kubis.jozef@outlook.com")
-  const [password, setPassword] = useState("netopier")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const { login, isLoading } = useLogin()
 
   function handleSubmit(e) {

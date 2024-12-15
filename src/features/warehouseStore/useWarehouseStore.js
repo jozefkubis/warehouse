f9b7d909e359f5ebe7275062014d88f1fa81e758
jozef kubis <kubis.jozef@outlook.com>
2024-12-15 11:59:31 +0100

    zmeneny apiWharehouseStore na apiWareHouseStore

--- src/features/warehouseStore/useWarehouseStore.js
+++ src/features/warehouseStore/useWarehouseStore.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
-import { getWarehouseStore } from "../../services/apiWarehouseStore"
+import { getWarehouseStore } from "../../services/apiWareHouseStore"
 
 export function useWarehouseStore() {
   const {
